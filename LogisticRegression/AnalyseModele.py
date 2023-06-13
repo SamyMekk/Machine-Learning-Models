76b42b470b25c86f21bd6f24bf57cbba18e663bb
Samy Mekkaoui <samymekkaoui77@gmail.com>
2023-06-13 21:54:52 +0200

    Ajout Modèle Régression Logistique

---
+++ LogisticRegression/AnalyseModele.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 15:36:10 2023
+
+@author: smekkaoui
+"""
+
+
+import streamlit as st
+import pandas as pd
+import os
+os.chdir(r"C:\Users\samym\OneDrive - GENES\Bureau\Machine Learning Models\LogisticRegression")
+from RegressionLogistique import *
+from PIL import Image
+
+
+st.title("Modèle de Régression Logistique pour la prédiction de Heart Disease pour la population de Cleveland")
+
+st.header("Voici à quoi ressemble les Données sur lesquels notre étude va se baser ")
+
+
+
+def user_input():
+    age=st.sidebar.number_input("Choississez l'âge de l'individu ",value= 40)
+    sexe=st.sidebar.selectbox("Choississez le sexe de l'individu",('Homme','Femme'))
+    cp=st.sidebar.selectbox("Choississez le type d'angine",("Angine stable","Angine Instable","Autres douleurs","Asymptomatique"))
+    trestbps=st.sidebar.number_input("Choississez la Tension Artérielle au repos de l'individu",value=120)
+    chol=st.sidebar.number_input("Choississez le niveau de cholestérol",value=230)
+    fbs=st.sidebar.selectbox("L'individu a-t-il un taux de glycémie à jeun",('Oui','Non'))
+    restecg=st.sidebar.selectbox("Choississez le niveau de mesure d'électrocardiographie au repos",("Normal","Hypertrophie","Anomalies"))
+    thalach=st.sidebar.number_input("Choissisez la fréquence cardiaque maximale atteinte",value=150)
+    exang=st.sidebar.selectbox("Choissisez si l'exercice induit une angine",("Oui","Non"))
+    oldpeak=st.sidebar.number_input("Choississez la valeur d'oldpeak",value=0.5)
+    slope=st.sidebar.selectbox("Choissisez le risque lié à la slope",("En Hausse","Stable","En Baisse"))
+    ca=st.sidebar.selectbox("Choississez le risque lié aux problèmes cardiovasculaires",("Absence d'Anomalie","Faible","Moyen","Elevé"))
+    thal=st.sidebar.selectbox("Choissisez le risque lié à la Thalassémie",("Non","Thalassémie sous contrôle","Thalassémie instable"))
+    data={"Age":age,
+        "Sexe":sexe,
+        "cp":cp,
+        "trestbps":trestbps,
+        "chol":chol,
+        "fbs":fbs,
+        "restecg":restecg,
+        "thalach":thalach,
+        "exang":exang,
+        "oldpeak":oldpeak,
+        "slope":slope,
+        "ca":ca,
+        "thal":thal,}
+    Parametres=pd.DataFrame(data,index=["Caractéristiques"])
+    return Parametres
+    
+
+
+st.header("Voici les résultats de la matrice de confusion :")
+image = Image.open(r'C:\Users\samym\OneDrive - GENES\Bureau\Machine Learning Models\LogisticRegression\data\MatrixConfusion.png')
+
+st.image(image, caption='Confusion Matrix')
+
+
+st.header("Voici les résultats pour la courbe ROC :")
+
+image = Image.open(r'C:\Users\samym\OneDrive - GENES\Bureau\Machine Learning Models\LogisticRegression\data\ROCCurve.png')
+
+st.image(image, caption='Courbe ROC')
+# Modèle Prédiction
+
+
+df=user_input()
+
+st.subheader("Voici les caractéristiques de l'individu : ")
+
+st.dataframe(df.transpose())
+
+DataforCalcul=DataTest.iloc[0:,0:len(DataTest.transpose())-1]
+
+DataForTest=pd.concat([DataforCalcul,df])
+
+Input=pd.get_dummies(DataForTest, columns=['Sexe',"cp","fbs","restecg","exang","slope","ca","thal"], drop_first=True)
+
+
+Candidat=pd.DataFrame(Input.iloc[-1,:]).transpose()
+
+st.header("Résultat de la prédiction par régression logistique : ")
+Prediction={"Prédiction":[np.round(classifier.predict_proba(Candidat)[0][0],4)]}
+Resultat=pd.DataFrame.from_dict(data=Prediction)
+Resultat.index=["Probabilité de ne pas subir d'attaque cardiaque :"]
+st.dataframe(Resultat)
---
+++ LogisticRegression/RegressionLogistique.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 15:25:47 2023
+
+@author: smekkaoui
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import *
+import matplotlib.pyplot as plt
+
+
+
+# Traitement des Données
+
+Dataset=pd.read_csv(r"C:\Users\samym\OneDrive - GENES\Bureau\Machine Learning Models\LogisticRegression\data\Cleveland.csv",index_col=0)
+Dataset.columns=["Age","Sexe","cp","trestbps","chol","fbs","restecg","thalach","exang","oldpeak","slope","ca","thal","target"]
+Data=Dataset.replace('?', np.NaN).copy()
+Data["thal"]=Data["thal"].astype("float64")
+Data["ca"]=Data["ca"].astype("float64")
+
+# Check des Données
+#print(Data.dtypes)
+
+# On supprime les valeurs manquantes
+
+Data=Data.dropna()
+
+# Recodage Variable
+def Target(x):
+    if x==1 or x==2 or x==3 or x==4:
+        return "Oui"
+    else:
+        return "Non"
+    
+def sex(x):
+    if x==float(0):
+        return "Femme"
+    else:
+        return "Homme"
+
+def cp(x):
+    if x==1:
+        return "Angine stable"
+    elif x==2:
+        return "Angine Instable"
+    elif x==3:
+        return "Autres douleurs"
+    else:
+        return "Asymptomatique"
+    
+def fbs(x):
+    if x==0:
+        return "Non"
+    else:
+        return "Oui"
+    
+def restecg(x):
+    if x==0:
+        return "Normal"
+    elif x==1:
+        return "Anomalies"
+    else:
+        return "Hypertrophie"
+    
+def exang(x):
+    if x==0:
+        return "Non"
+    else:
+        return "Oui"
+    
+def slope(x):
+    if x==1:
+        return "En Hausse"
+    elif x==2:
+        return "Stable"
+    else:
+        return "En Baisse"
+    
+def ca(x):
+    if x==0:
+        return "Absence d'Anomalie"
+    elif x==1:
+        return "Faible"
+    elif x==2:
+        return "Moyen"
+    else:
+        return "Elevé"
+    
+def thal(x):
+    if x==float(3):
+        return "Non"
+    elif x==float(6):
+        return "Thalassémie sous contrôle"
+    else:
+        return "Thalassémie instable"
+   
+    
+# Apply Fonctions Modèles
+
+Data["Sexe"]=Data["Sexe"].apply(sex)
+Data["cp"]=Data["cp"].apply(cp)
+Data["target"]=Data["target"].apply(Target)
+Data["fbs"]=Data["fbs"].apply(fbs)
+Data["restecg"]=Data["restecg"].apply(restecg)
+Data["exang"]=Data["exang"].apply(exang)
+Data["slope"]=Data["slope"].apply(slope)
+Data["ca"]=Data["ca"].apply(ca)
+Data["thal"]=Data["thal"].apply(thal)
+
+
+
+DataTest=Data.copy()
+# DataTest
+
+DataClean = pd.get_dummies(DataTest, columns=['Sexe',"cp","fbs","restecg","exang","slope","ca","thal","target"], drop_first=True)
+X=DataClean.drop(columns=['target_Oui'])
+y=DataClean["target_Oui"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+classifier = LogisticRegression(random_state = 0, penalty = 'none')
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_train)
+accuracy_score(y_train, y_pred)
+
+# Matrice de Confusion
+
+ConfusionMatrixDisplay.from_estimator(classifier,X_test,y_test)
+
+
+# Plot Courbe ROC
+
+RocCurveDisplay.from_estimator(classifier,X_test,y_test)
